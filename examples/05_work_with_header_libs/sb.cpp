07ce6371defdc0cc6b0b293e85da1414ecfe5539
tor4z <vwenjie@hotmail.com>
2026-07-18 23:54:01 +0800

    Update example with new api

--- examples/05_work_with_header_libs/sb.cpp
+++ examples/05_work_with_header_libs/sb.cpp
@@ -6,13 +6,7 @@ int main(int argc, char** argv)
     sb::auto_rebuild_self(argc, argv);
 
     auto exe = sb::create_exe("05_exe")
-        .add_srcs("main.cpp", "llm.cpp")
-        .add_flags("--target=aarch64-linux-gnu")
-        .set_compiler("clang++-22")
+        .add_srcs({"main.cpp", "llm.cpp"})
+        .set_compiler("clang++")
         .build();
-
-    if (exe.status() == 0) {
-        std::cout << "Push bin file..\n";
-        return sb::run_command("scp", exe.name.c_str(), "tor4z@192.168.1.131:~", nullptr);
-    }
 }
